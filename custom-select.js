14371d6c90bfc045b5fcf6f1202c7a4622e8060b
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2026-08-16 22:51:21 -0300

    Refactor mobile layouts structurally (#51)

--- custom-select.js
+++ custom-select.js
@@ -1,6 +1,6 @@
 // Reusable pill-style dropdown used throughout the admin forms and profile setup.
 // Ported 1:1 from the design's CustomSelect.dc.html component.
-import { Component, html } from './vendor/htm-preact-standalone.js?v=20260817-4';
+import { Component, html } from './vendor/htm-preact-standalone.js?v=20260817-5';
 
 let selectId = 0;
 
